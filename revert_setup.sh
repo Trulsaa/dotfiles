5391859bd8b0cd1354a3de702b4e10d243186eef
Truls <truls.aa@gmail.com>
2017-11-16 23:34:10 +0100

    Made big changes to setup.sh and tested them. Also created revert_setup.sh

---
+++ revert_setup.sh
@@ -0,0 +1,49 @@
+#!/bin/sh
+
+# This scipt unistalls and deletes all files introduced by the setup script
+
+# Delete oh-my-zsh
+echo "Removing ~/.oh-my-zsh"
+if [ -d ~/.oh-my-zsh ]; then
+  rm -rf ~/.oh-my-zsh
+fi
+echo "Looking for original zsh config..."
+if [ -f ~/.zshrc.pre-oh-my-zsh ] || [ -h ~/.zshrc.pre-oh-my-zsh ]; then
+  echo "Found ~/.zshrc.pre-oh-my-zsh -- Restoring to ~/.zshrc";
+
+  if [ -f ~/.zshrc ] || [ -h ~/.zshrc ]; then
+    ZSHRC_SAVE=".zshrc.omz-uninstalled-$(date +%Y%m%d%H%M%S)";
+    echo "Found ~/.zshrc -- Renaming to ~/${ZSHRC_SAVE}";
+    mv ~/.zshrc ~/"${ZSHRC_SAVE}";
+  fi
+
+  mv ~/.zshrc.pre-oh-my-zsh ~/.zshrc;
+
+  echo "Your original zsh config was restored. Please restart your session."
+else
+  if hash chsh >/dev/null 2>&1; then
+    echo "Switching back to bash"
+    chsh -s /bin/bash
+  else
+    echo "You can edit /etc/passwd to switch your default shell back to bash"
+  fi
+fi
+
+# Move dotfiles to the trash
+echo "Moving dotfiles to the trash"
+trash ~/.config
+trash ~/.bash_profile
+trash ~/.eslintrc
+trash ~/.gitconfig
+trash ~/.global_ignore
+trash ~/.oh-my-zsh.sh
+trash ~/.tmux.conf
+trash ~/.zshenv
+trash ~/.zshrc
+
+brew gem uninstall coderay
+brew gem uninstall rouge
+
+pip3 uninstall neovim
+
+brew uninstall wget tmux node neovim python2 brew-gem python3 tidy-html5 tree ranger trash highlight reattach-to-user-namespace
--- setup.sh
+++ setup.sh
@@ -3,63 +3,54 @@
 # Installation notes
 
 # 1. Install iTerm from website
-# 2. Run the following command:
+# 2. Run the following command which installs Homebrew and git:
 # /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)" && brew install git
 # 3. Download this repo
-# 4. Run this script from this repo from the ropo folder.
+# 4. Run this script from this repos folder.
 
-# Install wget
-brew install wget
+# Update gem
+sudo /usr/bin/gem update --system
 
-# Install oh-my-zsh
-sh -c "$(wget https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
-
-# Install tmux
-brew install tmux
-
-# Install node
-brew install node
-
-# Install neovim 
-brew install neovim
-
-# Install pip3
-brew install python3
+# Have brew install:
+# wget - for retrieving files using HTTP, HTTPS, FTP and FTPS
+# tmux - terminal multiplexer
+# node - JavaScript runtime language support
+# neovim - literally the future of vim
+# python3 - Python runtime language support
+# tidy-html5 - HTML linting
+# tree - generate tree structures of your filesystem
+# ranger - A vim inspired filemanger
+# trash - Move files and folders to the trash
+brew install wget tmux node neovim python2 brew-gem python3 tidy-html5 tree ranger trash highlight reattach-to-user-namespace
 
 # Implement support for python plugins in Nvim
 pip3 install neovim
 
-# For Webdevelopment
-# Update tidy which is used by neomake for html linting
-brew install tidy-html5
+# Implement support for ruby gem in Nvim
+brew gem install neovim
 
 # For fzf preview window highlighting
-brew install highlight
-gem install coderay
-gem install rouge
-
-# Install tree
-brew install tree
-
-# Install ranger
-brew install ranger
+brew gem install coderay
+brew gem install rouge
 
 # Symlink dotfiles
 mkdir ~/.config
-ln -sf bash/bash_profile ~/.bash_profile
-ln -sf eslint/eslintrc ~/.eslintrc
-ln -sf git/gitconfig ~/.gitconfig
-ln -sf git/global_ignore ~/.global_ignore
+ln -sf $PWD/bash/bash_profile ~/.bash_profile
+ln -sf $PWD/eslint/eslintrc ~/.eslintrc
+ln -sf $PWD/git/gitconfig ~/.gitconfig
+ln -sf $PWD/git/global_ignore ~/.global_ignore
 mkdir ~/.config/nvim
-ln -sF nvim/UltiSnips ~/.config/nvim/UltiSnips
-ln -sF nvim/ftplugin ~/.config/nvim/ftplugin
-ln -sf nvim/init.vim ~/.config/nvim/init.vim
-ln -sF nvim/vimrc ~/.config/nvim/vimrc
-ln -sf oh-my-zsh/oh-my-zsh.sh ~/.oh-my-zsh.sh
+ln -sF $PWD/nvim/UltiSnips ~/.config/nvim/UltiSnips
+ln -sF $PWD/nvim/ftplugin ~/.config/nvim/ftplugin
+ln -sf $PWD/nvim/init.vim ~/.config/nvim/init.vim
+ln -sF $PWD/nvim/vimrc ~/.config/nvim/vimrc
+ln -sf $PWD/oh-my-zsh/oh-my-zsh.sh ~/.oh-my-zsh.sh
 mkdir ~/.config/ranger
-ln -sf ranger/rc.conf ~/.config/ranger/rc.conf
-ln -sf tmux/tmux.conf ~/.tmux.conf
-ln -sf zsh/zshenv ~/.zshenv
-ln -sf zsh/zshrc ~/.zshrc
+ln -sf $PWD/ranger/rc.conf ~/.config/ranger/rc.conf
+ln -sf $PWD/tmux/tmux.conf ~/.tmux.conf
+ln -sf $PWD/zsh/zshenv ~/.zshenv
+ln -sf $PWD/zsh/zshrc ~/.zshrc
 
+# Install oh-my-zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
