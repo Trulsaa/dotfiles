60a7652e06581138914feeb52f5b71c4b4ad56f6
Truls <truls.aa@gmail.com>
2018-02-15 09:45:50 +0100

    Added linuxSetup

--- linuxSetup.sh
+++ linuxSetup.sh
@@ -2,17 +2,12 @@
 
 # Installation notes
 
-# 1. Install iTerm from website
-# 2. Run the following command which installs Homebrew and git:
-# /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)" && brew install git
-# 3. Download this repo
-# 4. Run this script from this repos folder.
+# 1. Download this repo
+# 2. Run this script from this repos folder.
 
-# Update gem
-sudo /usr/bin/gem update --system
 
-# Have brew install many things
-brew install \
+# apt-get install many things
+apt-get install \
     wget \
     tmux \
     node \
@@ -30,43 +25,9 @@ brew install \
     wiki \
     z
 
-# QuickLook plugins
-brew cask install \
-    qlcolorcode \
-    qlstephen \
-    qlmarkdown \
-    quicklook-json \
-    qlprettypatch \
-    quicklook-csv \
-    betterzipql \
-    qlimagesize \
-    webpquicklook \
-    suspicious-package \
-    qlvideo
 
 # Implement support for python plugins in Nvim
 pip3 install neovim
-
-# Linters
-npm install -g eslint
-npm install jsonlint -g
-pip3 install vim-vint
-brew install shellcheck
-
-# Required by airbnb eslintrc
-(
-  export PKG=eslint-config-airbnb;
-  npm info "$PKG@latest" peerDependencies --json | \
-      command sed 's/[\{\},]//g ; s/: /@/g' | \
-      xargs npm install -g "$PKG@latest"
-)
-
-# Implement support for ruby gem in Nvim
-brew gem install neovim
-
-# For fzf preview window highlighting
-brew gem install coderay
-brew gem install rouge
 
 # Install the powerline fonts
 git clone https://github.com/powerline/fonts.git --depth=1
@@ -98,14 +59,6 @@ ln -sf "$PWD"/zsh/zshrc ~/.zshrc
 
 # Install Tmux plugin manager
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
-
-# Have brew cask install:
-# Google-chrome
-# Jottacloud
-brew cask install \
-    google-chrome \
-    dropbox \
-    jotta
 
 # Install oh-my-zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
