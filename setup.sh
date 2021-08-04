2f58bbe87726a165f4d76a5d4c818064497999bb
Truls Aagaard <truls@aagaard.io>
2021-08-04 14:57:14 +0200

    Extract lua code into its own vimrc.lua

--- setup.sh
+++ setup.sh
@@ -54,6 +54,7 @@ ln -sf "$PWD"/gitconfig ~/.gitconfig
 mkdir ~/.config
 mkdir ~/.config/nvim
 ln -sf "$PWD"/nvim/UltiSnips ~/.config/nvim/UltiSnips
+ln -sf "$PWD"/nvim/lua ~/.config/nvim/lua
 ln -sf "$PWD"/nvim/after/ftplugin ~/.config/nvim/after/ftplugin
 ln -sf "$PWD"/nvim/init.vim ~/.config/nvim/init.vim
 ln -sf "$PWD"/oh-my-zsh.sh ~/.oh-my-zsh.sh
