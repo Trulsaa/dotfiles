5c5929bd07cde36b114d2f955afd0b43e1f19f0c
Truls Aagaard <truls@aagaard.io>
2022-04-14 12:06:57 +0200

    Fix date snippet after snippets.lua update

--- nvim/lua/snippets.lua
+++ nvim/lua/snippets.lua
@@ -1,21 +1,15 @@
 local ls = require("luasnip")
 local s = ls.snippet
-local sn = ls.snippet_node
-local isn = ls.indent_snippet_node
 local t = ls.text_node
-local i = ls.insert_node
 local f = ls.function_node
-local c = ls.choice_node
-local d = ls.dynamic_node
-local r = ls.restore_node
-local events = require("luasnip.util.events")
 
 local date_header = function()
-  return os.date("## %A %Y-%m-%d")
+  return os.date("# %A %Y-%m-%d")
 end
 
-ls.snippets = {
-  markdown = {
+ls.add_snippets(
+  "markdown",
+  {
     s(
       {
         trig = "date",
@@ -28,7 +22,7 @@ ls.snippets = {
       }
     )
   }
-}
+)
 
 require("luasnip.loaders.from_snipmate").lazy_load()
 
