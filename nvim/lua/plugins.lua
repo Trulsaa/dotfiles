cd274c2f0b3c95bb8038eb8f03d6ee37f2760d0a
Truls Aagaard <truls@aagaard.io>
2022-04-14 16:03:01 +0200

    Set mappings in on_attach func for gitsigns

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -264,7 +264,30 @@ return require("packer").startup(
                 delete = {show_count = true},
                 topdelete = {show_count = true},
                 changedelete = {show_count = true}
-              }
+              },
+              on_attach = function(bufnr)
+                local function map(mode, lhs, rhs, opts)
+                  opts = vim.tbl_extend("force", {noremap = true, silent = true}, opts or {})
+                  vim.api.nvim_buf_set_keymap(bufnr, mode, lhs, rhs, opts)
+                end
+
+                -- Navigation
+                map("n", "]c", "&diff ? ']c' : '<cmd>Gitsigns next_hunk<CR>'", {expr = true})
+                map("n", "[c", "&diff ? '[c' : '<cmd>Gitsigns prev_hunk<CR>'", {expr = true})
+
+                -- Actions
+                map("n", "<leader>hs", "<cmd>Gitsigns stage_hunk<CR>")
+                map("v", "<leader>hs", "<cmd>Gitsigns stage_hunk<CR>")
+                map("n", "<leader>hr", "<cmd>Gitsigns reset_hunk<CR>")
+                map("v", "<leader>hr", "<cmd>Gitsigns reset_hunk<CR>")
+                map("n", "<leader>hu", "<cmd>Gitsigns undo_stage_hunk<CR>")
+                map("n", "<leader>hp", "<cmd>Gitsigns preview_hunk<CR>")
+                map("n", "<leader>hb", '<cmd>lua require"gitsigns".blame_line{full=true}<CR>')
+
+                -- Text object
+                map("o", "ih", ":<C-U>Gitsigns select_hunk<CR>")
+                map("x", "ih", ":<C-U>Gitsigns select_hunk<CR>")
+              end
             }
           )
         end
