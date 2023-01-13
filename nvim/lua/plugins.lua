38588f0b672a84f3e127d000d37127125f87b4e6
Truls Aagaard <truls@aagaard.io>
2023-01-13 07:37:05 +0100

    Change auto save plugin

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -35,11 +35,45 @@ return require("packer").startup(
     use("wincent/loupe") -- More resonable search settings
     use(
       {
-        "Pocco81/AutoSave.nvim",
+        "Pocco81/auto-save.nvim",
         config = function()
-          require("autosave").setup(
+          require("auto-save").setup(
             {
-              execution_message = ""
+              enabled = true, -- start auto-save when the plugin is loaded (i.e. when your package manager loads it)
+              execution_message = {
+                message = function()
+                  -- message to print on save
+                  return ("AutoSave: saved at " .. vim.fn.strftime("%H:%M:%S"))
+                end,
+                dim = 0.18, -- dim the color of `message`
+                cleaning_interval = 1250 -- (milliseconds) automatically clean MsgArea after displaying `message`. See :h MsgArea
+              },
+              trigger_events = {"InsertLeave", "TextChanged"}, -- vim events that trigger auto-save. See :h events
+              -- function that determines whether to save the current buffer or not
+              -- return true: if buffer is ok to be saved
+              -- return false: if it's not ok to be saved
+              condition = function(buf)
+                local fn = vim.fn
+                local utils = require("auto-save.utils.data")
+
+                if
+                  fn.getbufvar(buf, "&modifiable") == 1 and
+                    utils.not_in(fn.getbufvar(buf, "&filetype"), {"TelescopePrompt"})
+                 then
+                  return true -- met condition(s), can save
+                end
+                return false -- can't save
+              end,
+              write_all_buffers = false, -- write all buffers when the current one meets `condition`
+              debounce_delay = 135, -- saves the file at most every `debounce_delay` milliseconds
+              callbacks = {
+                -- functions to be executed at different intervals
+                enabling = nil, -- ran when enabling auto-save
+                disabling = nil, -- ran when disabling auto-save
+                before_asserting_save = nil, -- ran before checking `condition`
+                before_saving = nil, -- ran before doing the actual save
+                after_saving = nil -- ran after doing the actual save
+              }
             }
           )
         end
@@ -54,12 +88,6 @@ return require("packer").startup(
         cmd = {"MarkdownPreview", "MarkdownPreviewToggle"}
       }
     )
-    use("kosayoda/nvim-lightbulb") -- show a lightbulb in the sign column if there are actions
-
-    -- Improved mouse support
-    -- Focus reporting (Reload buffer on focus if it has been changed externally )
-    -- Bracketed Paste mode
-    use("vim-scripts/vim-auto-save") -- Enables auto save
     use("ntpeters/vim-better-whitespace") -- Highlight trailing whitespace in red
     use("editorconfig/editorconfig-vim") -- Makes use of editorconfig files
     use("tpope/vim-projectionist") -- Projection and alternate navigation
