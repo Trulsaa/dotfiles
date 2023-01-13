4cbeca42c3a3a6648b6f021e469e26e4b24fe406
Truls Aagaard <truls@aagaard.io>
2023-01-13 07:48:29 +0100

    Extend telescope with fzf

--- nvim/lua/telescopesetup.lua
+++ nvim/lua/telescopesetup.lua
@@ -20,18 +20,21 @@ require("telescope").setup(
         flex = {
           flip_columns = 120
         }
-      },
-      mappings = {
-        i = {
-          ["<C-q>"] = require("telescope.actions").smart_send_to_qflist + require("telescope.actions").open_qflist
-        },
-        n = {
-          ["<C-q>"] = require("telescope.actions").smart_send_to_qflist + require("telescope.actions").open_qflist
-        }
+      }
+    },
+    extensions = {
+      fzf = {
+        fuzzy = true, -- false will only do exact matching
+        override_generic_sorter = true, -- override the generic sorter
+        override_file_sorter = true, -- override the file sorter
+        case_mode = "smart_case" -- or "ignore_case" or "respect_case"
+        -- the default case_mode is "smart_case"
       }
     }
   }
 )
+
+require("telescope").load_extension("fzf")
 
 local select_layout = function(builtin, opts)
   if not opts then
