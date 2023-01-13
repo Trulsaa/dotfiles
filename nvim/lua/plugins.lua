f7e9e9580a02b5a88a19f01186a1bd13c783a780
Truls Aagaard <truls@aagaard.io>
2023-01-13 07:38:11 +0100

    Specify telescop version and speed it up with fzf

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -259,10 +259,15 @@ return require("packer").startup(
     use(
       {
         "nvim-telescope/telescope.nvim",
+        branch = "0.1.x",
         requires = {"nvim-lua/plenary.nvim"},
         run = [[ brew install ripgrep ]]
       }
     )
+    use {
+      "nvim-telescope/telescope-fzf-native.nvim",
+      run = "cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build"
+    }
     use(
       {
         "nvim-telescope/telescope-ui-select.nvim",
