4a741711c4f3959ba3328feaa88fd767d6c002a2
Truls Aagaard <truls@aagaard.io>
2023-04-17 14:34:08 +0200

    Remove markdown preivew

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -78,15 +78,6 @@ return require("packer").startup(
         end
       }
     )
-    use(
-      {
-        "iamcco/markdown-preview.nvim",
-        run = "cd app && yarn install",
-        opt = true,
-        ft = {"markdown"},
-        cmd = {"MarkdownPreview", "MarkdownPreviewToggle"}
-      }
-    )
     use("ntpeters/vim-better-whitespace") -- Highlight trailing whitespace in red
     use("editorconfig/editorconfig-vim") -- Makes use of editorconfig files
     use("tpope/vim-projectionist") -- Projection and alternate navigation
