c8e8b15a2f5ce06810f4cbbe24eed1dfbdb95429
Truls Aagaard <truls@aagaard.io>
2024-01-02 15:54:39 +0100

    Update after using it

--- setup.sh
+++ setup.sh
@@ -5,6 +5,7 @@
 # Clone this repo - git clone https://github.com/Trulsaa/dotfiles.git
 # !!!. THEN RUN THIS SCRIPT FROM WITHIN THE REPO FOLDER. (because of the symlinking)
 
+brew tap simplydanny/pass-extensions
 # Have brew install many things
 brew install --no-quarantine \
   git-lfs \
@@ -18,7 +19,7 @@ brew install --no-quarantine \
   alacritty \
   pass \
   pass-update \
-  pass-odt \
+  pass-otp \
   pass-git-helper
 
 brew tap homebrew/cask-fonts
@@ -36,7 +37,7 @@ ln -sf "$PWD"/nvim/init.lua ~/.config/nvim/init.lua
 ln -sf "$PWD"/oh-my-zsh.sh ~/.oh-my-zsh.sh
 ln -sf "$PWD"/tmux.conf ~/.tmux.conf
 mkdir ~/.config/alacritty
-ln -sf "$PWD"/alacritty.yml ~/.config/alacritty/alacritty.yml
+ln -sf "$PWD"/alacritty.toml ~/.config/alacritty/alacritty.toml
 mkdir ~/.config/pass-git-helper
 ln -sf "$PWD"/git-pass-mapping.ini ~/.config/pass-git-helper/git-pass-mapping.ini
 
@@ -48,3 +49,4 @@ sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/mas
 
 cp "$PWD"/cobalt2.zsh-theme ~/.oh-my-zsh/themes
 ln -sf "$PWD"/zshrc ~/.zshrc
+git clone https://github.com/Aloxaf/fzf-tab ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/fzf-tab
